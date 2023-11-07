7df58c5f8c71f1c20e776d4804cffc856060c7e4
seiuna <nmmlwc@gmail.com>
2023-11-07 17:59:44 +0800

    l

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header style="background-color: rgb(52,101,250);    position:fixed;;
+  width: 100%;
+  top: 0;
+  left: 0;" >
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+    <div class="wrapper" >
+      <nav >
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <RouterView style="padding-top: 3rem; overflow: scroll"/>
 </template>
 
 <style scoped>
@@ -26,16 +27,11 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
-  width: 100%;
+
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  height: 3rem;
 }
 
 nav a.router-link-exact-active {
---
+++ src/views/LoginView.vue
@@ -0,0 +1,65 @@
+<template>
+
+
+    <el-form
+        :label-position="labelPosition"
+        label-width="100px"
+        :model="form"
+        style="max-width: 460px"
+        class="form"
+    >
+        <el-form-item label="角色">
+            <el-select v-model="form.role" class="m-2" placeholder="Select" size="large" >
+                <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                />
+            </el-select>
+        </el-form-item>
+        <el-form-item label="工号" v-show="form.role==='督导'">
+            <el-input v-model="form.region" />
+        </el-form-item>
+        <el-form-item label="学号" v-show="form.role==='学生'">
+            <el-input v-model="form.type" />
+        </el-form-item>
+        <el-form-item label="姓名" v-show="form.role==='学生'">
+            <el-input v-model="form.type" />
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">确定</el-button>
+            <el-button>取消</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+<style>
+.form{
+    margin: 20px;
+}
+</style>
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { FormProps } from 'element-plus'
+
+const labelPosition = ref<FormProps['labelPosition']>('top')
+
+const form = reactive({
+    name: '',
+    region: '',
+    type: '',
+    role:'学生'
+})
+
+const options = ref([  {
+    value: '学生',
+    label: '学生',
+},{
+    value: '督导',
+    label: '督导',
+}])
+
+const onSubmit=()=>{
+
+}
+</script>
